2bd36e9f3d693fc49ea3a1e68e06e637d083b875
CarlitosPJYT950 <juanestebanpradaospina@gmail.com>
2026-02-13 15:55:11 -0500

    git init

---
+++ src/src/src/Pilas.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.util.Stack;
+
+public class Pilas {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Stack<Integer> pila = new Stack<Integer>();
+        
+        System.out.println("Lista vacia: " + pila);
+        System.out.println("Esta vacia: " + pila.isEmpty());
+        
+        pila.push(1);
+        pila.push(2);
+        pila.push(3);
+        pila.push(4);
+        pila.push(5);
+        
+        for(Integer pilita : pila){System.out.println(pilita);}
+        
+        System.out.println("Pila: " + pila);
+        System.out.println("Pila vacia: " + pila.isEmpty());
+        
+        pila.pop();
+        
+        System.out.println("Esta el 3: " + pila.search(3));
+        System.out.println("Ultimo agregado: " + pila.peek());
+    }
+}
