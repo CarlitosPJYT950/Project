bb74b51d40c2518451dccb3f5c412570f09b7f1a
CarlitosPJYT950 <juanestebanpradaospina@gmail.com>
2026-02-13 19:02:13 -0500

    git commit

--- public/src/pkgpublic/Public.java
+++ public/src/pkgpublic/Public.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pkgpublic;
 
-/**
- *
- * @author LENOVO
- */
-public class Public {
+import java.util.HashMap;
+import java.util.Map;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Public {
     public static void main(String[] args) {
         // TODO code application logic here
+        Map<Integer,String> empleados = new HashMap<>();
+        
+        empleados.put(1, "Juan");
+        empleados.put(2, "Esteban");
+        empleados.put(3, "Juan Esteban");
+        empleados.put(4, "Ospina");
+        empleados.put(5, "Moreno");
+        empleados.put(6, "Ospina Moreno");
+        
+        boolean name = empleados.containsValue("Juliana");
+        
+        if(name == true){System.out.println("El valor buscado esta");}
+        else{System.out.println("El valor buscado no esta");}
+        
+        boolean number = empleados.containsKey(3);
+        
+        if(number == true){System.out.println("El valor buscado esta");}
+        else{System.out.println("El valor buscado no esta");}
+        
+        System.out.println(empleados.keySet());
+        System.out.println(empleados.values());
     }
-    
 }
